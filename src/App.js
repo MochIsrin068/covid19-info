31dfa8f8041a143a9cf5802d2f18af3c20adf5d5
Muhammad Isrim <isrim@doogether.id>
2020-03-07 16:13:35 +0800

    covid19 info

--- src/App.js
+++ src/App.js
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Row, Col, Card, Spin, Tooltip} from 'antd'
+import 'antd/dist/antd.css'
+import './covid.css'
+import Happy from './assets/emoji.png'
+import Sad from './assets/sad.png'
+import Verysad from './assets/verysad.png'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Confirmed from './pages/Confirmed'
+import Recovered from './pages/Recovered'
+import Deaths from './pages/Death'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+
+  state = {
+    isLoading : true,
+    data : null,
+    currentPage : "/",
+  }
+
+  componentDidMount(){
+    this.fetchAPI("https://covid19.mathdro.id/api/")
+  }
+
+  fetchAPI(url){
+    try {
+      fetch(url)
+      .then(res => res.json())
+      .then(jsondata => {
+        this.setState({
+          isLoading : false,
+          data : jsondata
+        })
+        console.log("Data", this.state.data)
+      })
+    } catch (error) {
+      console.log("Ngacoh")
+    }
+  }
+
+
+  handleCLick(page){
+    this.setState({
+      currentPage : page,
+    })
+
+    // this.fetchAPI(url)
+  }
+
+
+  render(){
+    const {isLoading, data} = this.state
+
+    if(isLoading){
+      return (
+        <div className="site-card-wrapper">
+          <center><Spin size="large"/></center>
+        </div>
+      )
+    }else{
+      return (
+        <Router>
+            <Switch>
+              <Route path="/confirmed">
+                <Confirmed/>
+              </Route>
+              <Route path="/recovered">
+                <Recovered/>
+              </Route>
+              <Route path="/death">
+                <Deaths />
+              </Route>
+              <Route path="/">
+                  <div className="site-card-wrapper">
+                    <Row style={{marginBottom : 20}}>
+                      <Col span={24}>
+                        <center>
+                          <h1>COVID19 INFORMATION</h1>
+                          <h4>Corona Virus data Information, confirmed, recovered and deaths, keep safe and smile :)</h4>
+                        </center>
+                      </Col>
+                    </Row>
+                    <Row gutter={16}>
+                      <Col span={8}>
+                        <Tooltip placement="bottom" title={<span>Click to Show Corona Virus Confirmed</span>}>
+                          <Link to="/confirmed" onClick={() => this.handleCLick("/confirmed")}>
+                            <Card title="CONFIRMED" bordered={false} className="card" >
+                              <img src={Sad} style={{width : '10%', marginBottom : 10}}/>
+                              <h1>{data.confirmed.value}</h1>
+                            </Card>
+                          </Link>
+                        </Tooltip>
+                      </Col>
+                      <Col span={8}>
+                        <Tooltip placement="bottom" title={<span>Click to Show Corona Virus Recovered</span>}>
+                          <Link to="/recovered" onClick={() => this.handleCLick("/recovered")}>
+                            <Card title="RECOREVERED" bordered={false} className="card">
+                              <img src={Happy} style={{width : '10%',  marginBottom : 10}}/>
+                              <h1 style={{color: "green"}}>{data.recovered.value}</h1>
+                            </Card>
+                          </Link>
+                        </Tooltip>
+                      </Col>
+                      <Col span={8}>
+                        <Tooltip placement="bottom" title={<span>Click to Show Corona Virus total Death</span>}>
+                          <Link to="/death" onClick={() => this.handleCLick("/death")}>
+                            <Card title="DEATH" bordered={false} className="card">
+                              <img src={Verysad} style={{width : '10%',  marginBottom : 10}}/>
+                              <h1 style={{color: "red"}}>{data.deaths.value}</h1>
+                            </Card>
+                          </Link>
+                        </Tooltip>
+                      </Col>
+                    </Row>
+                </div>
+              </Route>
+            </Switch>
+        </Router>
+      )
+    }
+  }
 }
-
-export default App;
---
+++ src/pages/Recovered.js
@@ -0,0 +1,144 @@
+import React from 'react'
+import {Spin, Row , Col, Card, Input, Button, Dropdown, Menu, message} from 'antd'
+import { ArrowLeftOutlined, DownOutlined, SyncOutlined, FlagOutlined, EnvironmentTwoTone } from '@ant-design/icons';
+import '../covid.css'
+import {Link} from 'react-router-dom'
+
+const {Search}= Input 
+
+export default class Recovered extends React.Component{
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            data : null,
+            isLoading :true,
+            search : ""
+        }
+    }
+
+    componentDidMount(){
+        try {
+            fetch("https://covid19.mathdro.id/api/recovered")
+            .then(res => res.json())
+            .then(jsonData => {
+                console.log("Confirmed", jsonData[0])
+                this.setState({
+                    isLoading : false,
+                    data : jsonData
+                })
+            })
+        } catch (error) {
+            console.log("Ngacoh")
+        }
+    }
+
+    handleChange = (e) => {
+        const key = e.target.value
+
+        this.setState({search : key})
+    }
+
+    handleMenuClick = (e) => {
+        const country = this.state.data.map(data => {
+            return data
+        })
+
+        let countryCompare = Object.values(country.reduce((c, {countryRegion}) => {
+            c[countryRegion] = c[countryRegion] || {countryRegion}
+            return c
+        }, {}))
+
+        let selectCountry = countryCompare.filter((c, i) =>{
+            return e.key == i
+        })
+
+        this.setState({
+            search : selectCountry[0].countryRegion
+        })
+    }
+
+    refresh = () => {
+        this.setState({search : ""})
+    }
+
+
+    render(){
+        const {isLoading, data, search} = this.state
+
+        if(isLoading){
+            return(
+                <center>
+                    <Spin size="large" />
+                </center>
+            )
+        }else{
+            const filteredData = data.filter(data => {
+                return data.countryRegion.toLowerCase().indexOf(search.toLowerCase()) !== -1
+            })
+
+            const country = data.map(data => {
+                return data
+            })
+
+            let countryCompare = Object.values(country.reduce((c, {countryRegion}) => {
+                c[countryRegion] = c[countryRegion] || {countryRegion}
+                return c
+            }, {}))
+
+            console.log("CountryRes", countryCompare)
+
+            const menu = (
+                <Menu onClick={(e) => this.handleMenuClick(e)}>
+                    {countryCompare.map((v, i) => {
+                        return (
+                            <Menu.Item key={i}>
+                                <FlagOutlined />
+                                {v.countryRegion}
+                            </Menu.Item>
+                        )
+                    })}
+                </Menu>
+            );
+
+            return (
+                <div className="site-card-wrapper-confirmed">
+                    <Row style={{marginBottom : 20}}>
+                        <Col span={22} push={2}>
+                            <Search placeholder="Type Your Country" onSearch={value => console.log(value)} enterButton size="large" onChange={(e) => this.handleChange(e)}/>
+                        </Col>
+                        <Col span={2} pull={22}>
+                            <Link to="/"><Button type="primary" size="large"><ArrowLeftOutlined twoToneColor="#fff"/></Button></Link>
+                        </Col>
+                    </Row>
+
+                  
+                    <Dropdown overlay={menu} style={{width : '90%'}}>
+                        <Button style={{width :'90%'}}>
+                            Filtered By Select Country<DownOutlined />
+                        </Button>
+                    </Dropdown>
+                    <Button  style={{width : '10%'}} type="primary" onClick={() => this.refresh()}><SyncOutlined spin twoToneColor="#fff"/></Button>
+              
+
+                    {filteredData.map((v, i) => {
+                        return (
+                        <Row style={{marginBottom : 20, marginTop : 20}} key={i}>
+                            <Col span={24}>
+                            <center>
+                                <Card title={v.countryRegion} bordered={false} className="card">
+                                    <h1 style={{color: "green"}}>{v.recovered}</h1>
+                                    <a href={`https://www.google.com.sa/maps/search/@${v.lat},${v.long},12.21z?hl=en`} target="_blank" rel="noopener">Open Maps&nbsp;<EnvironmentTwoTone twoToneColor="#52c41a" title="Open Maps"/></a>
+                                </Card>
+                            </center>
+                            </Col>
+                        </Row>
+                        )
+                    })}
+                </div>
+            )
+        }
+        
+    }
+}
